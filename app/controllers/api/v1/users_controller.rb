6613e7ead09b40f952295ec5e013141c14e21f3e
Suneel Kumar <suneel.kumar@nxb.com.pk>
2015-02-13 16:57:06 +0500

    Adds authorization for the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_with_token!, only: [:update, :destroy]
   respond_to :json
 
   def show
@@ -15,20 +16,19 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-	  user = User.find(params[:id])
+    user = current_user
 
-	  if user.update(user_params)
-	    render json: user, status: 200, location: [:api, user]
-	  else
-	    render json: { errors: user.errors }, status: 422
-	  end
-	end
+    if user.update(user_params)
+      render json: user, status: 200, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
 
-	def destroy
-	  user = User.find(params[:id])
-	  user.destroy
-	  head 204
-	end
+	 def destroy
+    current_user.destroy
+    head 204
+  end
 
   private
     def user_params
