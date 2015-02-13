a47d2eeaa6b7f479355ebfb32038a1a99af5a7e8
Suneel Kumar <suneel.kumar@nxb.com.pk>
2015-02-13 16:12:09 +0500

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ MarketPlaceApi::Application.routes.draw do
    # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
+      resources :users, :only => [:show, :create]
     end
   end
 
