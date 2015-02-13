563044569e53c5fe17caa6fe1c01c0b28cd95ffb
Suneel Kumar <suneel.kumar@nxb.com.pk>
2015-02-13 15:00:08 +0500

    Set the routes contrains for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 MarketPlaceApi::Application.routes.draw do
   
+   # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    # We are going to list our resources here
+  end
+
 end
