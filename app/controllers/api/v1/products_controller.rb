e33f719feae564aee2c2743cb81efd72b99a3e6f
Suneel Kumar <suneel.kumar@nxb.com.pk>
2015-02-13 19:59:33 +0500

    Refactors the pagination behavior for index actions

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,12 +1,9 @@
 class Api::V1::ProductsController < ApplicationController
   respond_to :json
 
-  def index
-    respond_with Product.search(params).page(params[:page]).per(params[:per_page])
-    render json: products, meta: { pagination:
-                                   { per_page: params[:per_page],
-                                     total_pages: products.total_pages,
-                                     total_objects: products.total_count } }
+   def index
+    products = Product.search(params).page(params[:page]).per(params[:per_page])
+    render json: products, meta: pagination(products, params[:per_page])
   end
    
 
